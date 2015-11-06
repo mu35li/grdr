b50b32a49398dee563eaf67d14cd4b9bd35bdbf2
Julian Deinert <j.deinert@gmx.net>
2015-11-06 23:53:29 +0100

    Add rotSpeed

--- src/game/main.js
+++ src/game/main.js
@@ -12,10 +12,11 @@ game.module(
             this.logo = new game.Sprite('logo.png').addTo(this.stage);
             this.logo.anchor.set(0.5, 0.5);
             this.logo.center();
+            this.rotSpeed = 1;
         },
 
         update: function() {
-            this.logo.rotation += (Math.PI * 2 / 10 * game.system.delta);
+            this.logo.rotation += (Math.PI * 2 / (1/this.rotSpeed) * game.system.delta);
         }
     });
 
