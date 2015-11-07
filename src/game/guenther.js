15a32660661fbbf7186ac20f3e08f07df36f56a5
Julian Deinert <j.deinert@gmx.net>
2015-11-07 14:52:08 +0100

    position guenther add neck

--- src/game/guenther.js
+++ src/game/guenther.js
@@ -1,6 +1,7 @@
 game.module('game.guenther')
 .body(function() {
 	game.addAsset('dude.png');
+	game.addAsset('neck.png');
 
 	game.createClass('Guenther', {
 		init: function(x, y, width, height) {
@@ -9,17 +10,25 @@ game.module('game.guenther')
 			this.width = width;
 			this.height = height;
 			this.jumpVel = 50;
-			this.sprite =  new game.Sprite('dude.png', this.x, this.y, {
+			this.head =  new game.Sprite('dude.png', this.x, this.y, {
 				anchor: {
 					x: 0.5,
 					y: 0.5
 				},
-				zIndex: 10
+				zIndex: 1
+			});
+			this.neck =  new game.Sprite('neck.png', this.x+2, this.y+87, {
+				anchor: {
+					x: 0.5,
+					y: 0.5
+				},
+				zIndex: 9 
 			});
 
 			game.scene.addObject(this);
 
-			this.sprite.addTo(game.scene.stage);
+			this.neck.addTo(game.scene.stage);
+			this.head.addTo(game.scene.stage);
 			
 			var shape = new game.Rectangle(this.width, this.height);
 			
@@ -37,7 +46,7 @@ game.module('game.guenther')
 		update: function() {
 			this.x = this.body.position.x;
 			this.y = this.body.position.y;
-			this.sprite.y = this.y;
+			this.head.y = this.y;
 		},
 
 		jump: function() {
@@ -45,8 +54,14 @@ game.module('game.guenther')
 		},
 
 		tiltHead: function(angle) {
-			console.log(angle);
-			this.sprite.rotation += angle;
+			console.log(this.head.rotation);	
+			if (((this.head.rotation + angle)<=2.4) && ((this.head.rotation + angle) >= -0.1)) {
+				this.head.rotation += angle;
+			}else if (this.head.rotation>2.4){
+				this.head.rotation = 2.4;
+			}else if (this.head.rotation < -0.1){
+				this.head.rotation = -0.1;
+			}
 		}
 
 	})
