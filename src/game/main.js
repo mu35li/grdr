d2a2353d0f2b7eafccbfccc13854340f18f9969a
tronje <tronjekrabbe@gmail.com>
2015-11-07 12:42:10 +0100

    add beer (currently, beer.js is redundant and will probably remain unused)

--- src/game/main.js
+++ src/game/main.js
@@ -2,6 +2,7 @@ game.module(
     'game.main'
 )
 .require('game.guenther')
+.require('game.beer')
 .body(function() {
 
     game.addAsset('logo.png');
@@ -44,6 +45,25 @@ game.module(
 
 
             // console.log(this.guenther);
+            
+            //this.beer = [];
+            //for (i = 0; i < 20; i++) {
+            //    this.beer.push(new game.Beer(50, 50, 5, 5));
+            //    this.world.addBody(this.beer[i].body);
+            //}
+
+            this.emitter = new game.Emitter();
+            this.emitter.life = 0;
+            this.emitter.velRotate = 25;
+            this.emitter.textures.push('beer.png');
+            this.emitter.position.set(100, 100);
+            this.emitter.positionVar.set(50, 50);
+            this.emitter.addTo(game.scene.stage);
+            game.scene.addEmitter(this.emitter);
+
+            for (i = 0; i < 20; i++) {
+                this.emitter.addParticle();
+            }
         },
 
         update: function() {
@@ -54,6 +74,8 @@ game.module(
             this.guenther.update();
             // console.log(this.guenther);
             // console.log("body pos: " + this.body.position.x + ", " + this.body.position.y);
+            
+            this.emitter.update();
 
             if (game.keyboard.down('RIGHT')) {
                 this.logo.rotation += (Math.PI * 2 / (1/this.rotSpeed) * game.system.delta);
