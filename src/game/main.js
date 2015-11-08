422f1900f63ab4aed5f74033f8f176dd67e4e9e0
tronje <tronjekrabbe@gmail.com>
2015-11-08 01:05:31 +0100

    redbull

--- src/game/main.js
+++ src/game/main.js
@@ -24,7 +24,7 @@ game.module(
 
             //instanciate the guenther
             this.guenther = new game.Guenther(850,600,35,35);
-            this.bottle = new game.Bottle(400,400,100,100);
+            this.bottle = new game.Bottle(450,400,100,100);
             // console.log(this.guenther);
             this.world.addBody(this.guenther.drinkBox);
 
